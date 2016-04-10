475dbb438b5d63c79c9984a0722771c2b1a947ec
Boaz <boaz@n3rd.nl>
2016-04-10 15:14:36 +0200

    initial commit

---
+++ LogstashTest/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogstashTest
+{
+    public class MyClass
+    {
+
+        static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public void DoSomeThing()
+        {
+            _logger.Debug("Debug test");
+
+            _logger.Warn("Warn test");
+
+            _logger.Error("Error test");
+
+            throw new Exception("thrown exception from MyClass");
+        }
+
+    }
+}
---
+++ LogstashTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogstashTest
+{
+    class Program
+    {
+        static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            _logger.Debug("Debug test");
+
+            _logger.Error("Error test");
+
+            try
+            {
+                new MyClass().DoSomeThing();
+            }
+            catch(Exception e)
+            {
+                _logger.Error(e, "Thrown error exception test");
+            }
+        }
+    }
+}
